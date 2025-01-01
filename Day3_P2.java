5ba1ded93f05e9aa8f2c8a4666f7bf7b31f1118b
Sam Lu <pbcmarsh@gmail.com>
2024-12-31 19:44:59 -0600

    Create Day3_P2.java

---
+++ Day3_P2.java
@@ -0,0 +1,29 @@
+import java.util.regex.*;
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    long ans = 0;
+	    String pattern = "mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)";
+    	boolean yes = true;
+	    while (sc.hasNextLine()) {
+    	    String s = sc.nextLine();
+    	    Pattern compiled = Pattern.compile(pattern);
+    	    Matcher matcher = compiled.matcher(s);
+    	    while (matcher.find()) {
+    	        String mul = matcher.group();
+    	        if (mul.equals("do()")) {
+    	            yes = true;
+    	        } else if (mul.equals("don't()")) {
+    	            yes = false;
+    	        } else if (yes) {
+    	            String[] ss = mul.substring(4, mul.length() - 1).split(",");
+    	            ans += Long.parseLong(ss[0]) * Long.parseLong(ss[1]);
+    	        }
+    	    }	        
+	    }
+	    System.out.println(ans);
+	}
+}
