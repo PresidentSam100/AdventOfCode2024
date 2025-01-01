99e433c61845248709bbd5ed1ee84a64ebf85092
Sam Lu <pbcmarsh@gmail.com>
2024-12-31 19:51:15 -0600

    Update Day3_P1.java

--- Day3_P1.java
+++ Day3_P1.java
@@ -8,14 +8,14 @@ public class Main
 	    long ans = 0;
 	    String pattern = "mul\\(\\d+,\\d+\\)";
 	    while (sc.hasNextLine()) {
-    	    String s = sc.nextLine();
-    	    Pattern compiled = Pattern.compile(pattern);
-    	    Matcher matcher = compiled.matcher(s);
-    	    while (matcher.find()) {
-    	        String mul = matcher.group();
-    	        String[] ss = mul.substring(4, mul.length() - 1).split(",");
-    	        ans += Long.parseLong(ss[0]) * Long.parseLong(ss[1]);
-    	    }	        
+    	        String s = sc.nextLine();
+    	        Pattern compiled = Pattern.compile(pattern);
+    	        Matcher matcher = compiled.matcher(s);
+    	        while (matcher.find()) {
+    	            String mul = matcher.group();
+    	            String[] ss = mul.substring(4, mul.length() - 1).split(",");
+    	            ans += Long.parseLong(ss[0]) * Long.parseLong(ss[1]);
+    	        }
 	    }
 	    System.out.println(ans);
 	}
